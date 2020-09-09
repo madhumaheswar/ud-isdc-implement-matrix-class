8e364b30aff1c7b429f7ecab76664a52d899a8e6
Madhu Maheswar B <madhumaheswar@gmail.com>
2020-09-10 01:10:16 +0530

    Implement Project

--- matrix.py
+++ matrix.py
@@ -18,6 +18,21 @@ def identity(n):
             I.g[i][i] = 1.0
         return I
 
+def dot_product(vectorA, vectorB):
+    """
+    Calulates the dot product of 2 vectors (lists)
+    Input : 2 lists of equal size
+    Ouput : dot product
+    """
+    if len(vectorA) != len(vectorB):
+        raise(ValueError, "The lengths of vector should be equal for dot product")
+        
+    d_p = 0
+    for i in range(len(vectorA)):
+        d_p += vectorA[i] * vectorB[i]
+        
+    return d_p
+
 class Matrix(object):
 
     # Constructor
@@ -39,8 +54,15 @@ class Matrix(object):
         if self.h > 2:
             raise(NotImplementedError, "Calculating determinant not implemented for matrices largerer than 2x2.")
         
-        # TODO - your code here
-
+        if self.h == 1:
+            return (self.g[0][0])    #determinant for 1x1 matrix
+        else:
+            a = self.g[0][0]
+            b = self.g[0][1]
+            c = self.g[1][0]
+            d = self.g[1][1]
+            return (a*d - b*c)    #determinant for 2x2 matrix
+        
     def trace(self):
         """
         Calculates the trace of a matrix (sum of diagonal entries).
@@ -48,7 +70,11 @@ class Matrix(object):
         if not self.is_square():
             raise(ValueError, "Cannot calculate the trace of a non-square matrix.")
 
-        # TODO - your code here
+        sum = 0
+        for i in range(self.h):
+            sum += self.g[i][i]
+        
+        return sum
 
     def inverse(self):
         """
@@ -58,14 +84,34 @@ class Matrix(object):
             raise(ValueError, "Non-square Matrix does not have an inverse.")
         if self.h > 2:
             raise(NotImplementedError, "inversion not implemented for matrices larger than 2x2.")
-
-        # TODO - your code here
-
+        
+        det = self.determinant()    #Calculates the determinant of the matrix
+        
+        if self.h == 1:
+            return Matrix([[1/det]])
+        else:
+            if det == 0:                 #Check if matrix has an Inverse
+                raise(ValueError, "Determinant doesn't exist")
+            else:
+                a = self.g[0][0] / det
+                b = self.g[0][1] / det
+                c = self.g[1][0] / det
+                d = self.g[1][1] / det
+                return Matrix([[d, -b], [-c, a]])
+    
     def T(self):
         """
         Returns a transposed copy of this Matrix.
         """
-        # TODO - your code here
+        transpose = []
+        
+        for column in range(self.w):
+            temp_row = []
+            for row in range(self.h):
+                temp_row.append(self.g[row][column])
+            transpose.append(temp_row)
+        
+        return Matrix(transpose)
 
     def is_square(self):
         return self.h == self.w
@@ -105,10 +151,17 @@ class Matrix(object):
         """
         if self.h != other.h or self.w != other.w:
             raise(ValueError, "Matrices can only be added if the dimensions are the same") 
-        #   
-        # TODO - your code here
-        #
-
+        
+        sum_matrix = []
+        
+        for row in range(self.h):
+            temp_row = []
+            for column in range(self.w):
+                temp_row.append(self.g[row][column] + other.g[row][column]) # add corresponding elements of both the matrices
+            sum_matrix.append(temp_row)
+        
+        return Matrix(sum_matrix)
+    
     def __neg__(self):
         """
         Defines the behavior of - operator (NOT subtraction)
@@ -121,25 +174,51 @@ class Matrix(object):
           -1.0  -2.0
           -3.0  -4.0
         """
-        #   
-        # TODO - your code here
-        #
+        negative = []
+        
+        for row in range(self.h):
+            temp_row = []
+            for column in range(self.w):
+                temp_row.append(-self.g[row][column])
+            negative.append(temp_row)
+                
+        return Matrix(negative)
 
     def __sub__(self, other):
         """
         Defines the behavior of - operator (as subtraction)
         """
-        #   
-        # TODO - your code here
-        #
+        if self.h != other.h or self.w != other.w:
+            raise(ValueError, "Matrices can only be subtracted if the dimensions are the same") 
+            
+        subtracted_matrix = []
+        
+        for row in range(self.h):
+            temp_row = []
+            for column in range(self.w):
+                temp_row.append(self.g[row][column] - other.g[row][column]) # add corresponding elements of both the matrices
+            subtracted_matrix.append(temp_row)
+        
+        return Matrix(subtracted_matrix)
 
     def __mul__(self, other):
         """
         Defines the behavior of * operator (matrix multiplication)
         """
-        #   
-        # TODO - your code here
-        #
+        if self.w != other.h:
+            raise(ValueError, "The column of 1st matrix should be equal to row of 2nd matrix for matrix multiplication")
+            
+        transpose_other = other.T()
+        
+        mat_product = []
+        
+        for rowA in self.g:
+            temp_row = []
+            for rowB in transpose_other.g:
+                temp_row.append(dot_product(rowA, rowB))
+            mat_product.append(temp_row)
+            
+        return Matrix(mat_product)
 
     def __rmul__(self, other):
         """
@@ -155,7 +234,14 @@ class Matrix(object):
         """
         if isinstance(other, numbers.Number):
             pass
-            #   
-            # TODO - your code here
-            #
+            mat_product = []
+            
+            for row in self.g:
+                temp_row = []
+                for element in row:
+                    temp_row.append(element * other)
+                mat_product.append(temp_row)
+                
+            return Matrix(mat_product)
+           
             
